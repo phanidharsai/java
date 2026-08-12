5ba479efed32f7ad5512f385c1dbce48391f7d84
phanidharsai <phanidharsai111@gmail.com>
2026-08-12 23:22:29 +0530

    practice dsa day 7

--- src/main/java/com/phanidharsai/dsa/binarySearchAlgoProblems/FindCeiling.java
+++ src/main/java/com/phanidharsai/dsa/binarySearchAlgoProblems/FindCeiling.java
@@ -9,14 +9,18 @@ public class FindCeiling {
             if(target==in[mid]){
                 return in[mid];
             }
-            if(target<in[mid]){
+            else if(target<in[mid]){
                     end=mid-1;
             }
             else if(target>in[mid]){
                 start= mid+1;
             }
         }
-        if(!(start>in.length-1)) {
+//        to find floor of target number use this
+//        if(end<in.length-1) {
+//            return in[end];
+//        }
+        if(start<in.length-1) {
             return in[start];
         }
         return -1;
@@ -24,7 +28,7 @@ public class FindCeiling {
 
     public static void main(String[] args){
         int[] arr ={2,3,5,9,14,16};
-        int num = 18;
+        int num = 12;
         int res = FindCeilingNum(arr, num);
         System.out.println(res);
     }
