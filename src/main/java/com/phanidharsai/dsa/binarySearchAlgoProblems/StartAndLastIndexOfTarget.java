9d2ef795c893e53dffe6e5cd9c3fb91098060fa5
phanidharsai <phanidharsai111@gmail.com>
2026-08-21 00:07:37 +0530

    practice dsa

--- src/main/java/com/phanidharsai/dsa/binarySearchAlgoProblems/StartAndLastIndexOfTarget.java
+++ src/main/java/com/phanidharsai/dsa/binarySearchAlgoProblems/StartAndLastIndexOfTarget.java
@@ -16,28 +16,26 @@ public class StartAndLastIndexOfTarget {
     }
     private static int searchIndex(int[] nums, int target, boolean isFirstIndex) {
         int start = 0;
-        int end = nums.length-1;
-        while(start<=end){
-            int mid = (start+end)/2;
-            if(nums[mid]<target){
-                start=mid+1;
-            }
-            else if(nums[mid]>target){
-                end = mid-1;
+        int end = nums.length - 1;
+        int ans = 0;
+        while (start <= end) {
+           int mid = (start + end) / 2;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
 
-            }
-            else{
-                if(isFirstIndex){
-                    end=mid-1;
+            } else {
+                ans=mid;
+                if (isFirstIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid +1;
                 }
-                else{
-                    start=mid+1;
-                }
-                return mid;
             }
 
         }
-        return -1;
+        return ans;
     }
     public static void main(String[] args){
         int[] nums ={5,7,7,7,8,8,10};
