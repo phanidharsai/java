43321204ee9f7300e59641f5a2ab7ea2df7c9e25
phanidharsai <phanidharsai111@gmail.com>
2026-08-20 23:56:59 +0530

    practice dsa

--- src/main/java/com/phanidharsai/dsa/binarySearchAlgoProblems/StartAndLastIndexOfTarget.java
+++ src/main/java/com/phanidharsai/dsa/binarySearchAlgoProblems/StartAndLastIndexOfTarget.java
@@ -1,38 +1,49 @@
 package com.phanidharsai.dsa.binarySearchAlgoProblems;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StartAndLastIndexOfTarget {
-    private static int[] getIndexes(int[] nums, int target) {
-        int[] res = {-1,-1};
+
+    private static int[] getIndexes(int[] nums, int target){
+        int[] res = new int[2];
+        int startIndex = searchIndex(nums, target, true);
+        int endIndex = searchIndex(nums, target, false);
+        res[0]=startIndex;
+        res[1]= endIndex;
+        return res;
+    }
+    private static int searchIndex(int[] nums, int target, boolean isFirstIndex) {
         int start = 0;
         int end = nums.length-1;
-        int startIndex =-1;
-        int endIndex =-1;
-        while(start<end){
+        while(start<=end){
             int mid = (start+end)/2;
-            if(nums[mid]==target){
-
-
-
-            }
             if(nums[mid]<target){
                 start=mid+1;
-
             }
             else if(nums[mid]>target){
                 end = mid-1;
 
             }
+            else{
+                if(isFirstIndex){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+                return mid;
+            }
+
         }
-        return res;
+        return -1;
     }
     public static void main(String[] args){
-        int[] nums ={5,7,7,8,8,10};
+        int[] nums ={5,7,7,7,8,8,10};
         int target =8;
         int[] res = getIndexes(nums,target);
-        System.out.println(res);
+        System.out.println(Arrays.toString(res));
     }
 
 
