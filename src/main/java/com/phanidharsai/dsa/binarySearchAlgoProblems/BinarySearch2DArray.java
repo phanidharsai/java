48fc3f1a78f24e6f1fb6a6722ec63c228b5223f4
phanidharsai <phanidharsai111@gmail.com>
2026-09-04 23:44:08 +0530

    practice dsa

--- src/main/java/com/phanidharsai/dsa/binarySearchAlgoProblems/BinarySearch2DArray.java
+++ src/main/java/com/phanidharsai/dsa/binarySearchAlgoProblems/BinarySearch2DArray.java
@@ -21,12 +21,20 @@ public class BinarySearch2DArray {
                 }
         }
 
-        return -1+"";
+        return -1+"," + -1;
     }
     public static void main(String[] args){
-        int[][] arr ={{10,21,32,40},{14,23,35,50},{18,30,39,60},{20,33,42,71}};
-        int target = 71;
-        String value = getIndex(arr, target);
+        int[][] arr ={{10,21,32,40},
+                      {14,23,35,50},
+                      {18,30,39,60},
+                      {20,33,42,71}};
+        // above code works for sorted matrix as well with sam e rime complexity
+        int[][] arr2 ={{3,6,7,11},
+                       {13,14,20,26},
+                       {28,31,35,41},
+                       {43,46,50,51}};
+        int target = 35;
+        String value = getIndex(arr2, target);
         System.out.println(value);
     }
 }
