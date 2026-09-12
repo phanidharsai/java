f34dee42e10b1e5707bd6ca67dbc2b5f5e83ce62
phanidharsai <phanidharsai111@gmail.com>
2026-09-12 15:30:26 +0530

    practice dsa

---
+++ src/main/java/com/phanidharsai/dsa/sorting/basicsorting/cyclicsortprob/FindMissingNumber.java
@@ -0,0 +1,26 @@
+package com.phanidharsai.dsa.sorting.basicsorting.cyclicsortprob;
+
+import static com.phanidharsai.dsa.sorting.basicsorting.BubbleSort.swap;
+
+public class FindMissingNumber {
+    static int findNumber(int[] arr){
+        int len = arr.length;
+        int res=0;
+        for(int i=0;i<arr.length;i++){
+                while( arr[i]<len &&  arr[i]!=i ){
+                    swap(arr,i,arr[i]);
+                }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=i){
+                res = i;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        int[] arr ={4,0,2,1};
+        int num = findNumber(arr);
+        System.out.println(num);
+    }
+}
