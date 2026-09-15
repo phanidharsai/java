4770b30b8a80f9f3a2fb9997a83fd8904cae0ee9
phanidharsai <phanidharsai111@gmail.com>
2026-09-16 00:40:57 +0530

    practice dsa

---
+++ src/main/java/com/phanidharsai/dsa/sorting/basicsorting/cyclicsortprob/FindAllMissingNumbers.java
@@ -0,0 +1,30 @@
+package com.phanidharsai.dsa.sorting.basicsorting.cyclicsortprob;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.phanidharsai.dsa.sorting.basicsorting.BubbleSort.swap;
+
+public class FindAllMissingNumbers {
+    static List<Integer> findAll(int[] arr){
+        List<Integer> res = new ArrayList<>();
+        int i =0;
+        while(i<arr.length){
+            while(arr[i]!=arr[arr[i]-1]){
+                swap(arr,i,arr[i]-1);
+            }
+            i++;
+        }
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]!=j+1){
+                res.add(j+1);
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args){
+         int[] arr={4,3,2,7,8,2,3,1};
+         List<Integer> res = findAll(arr);
+         System.out.println(res.toString());
+    }
+}
